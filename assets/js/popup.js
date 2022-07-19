fb3d975b194d3a0f8fc6c5eeacddf4cb796fa555
niharikasitomer <ni.sitomer@berkeley.edu>
2022-07-18 22:03:34 -0700

    made automatic + added popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,45 +1,29 @@
-const text = document.getElementById( 'notify-text' );
-const notify = document.getElementById( 'notify-button' );
-const reset = document.getElementById( 'notify-reset' );
-const counter = document.getElementById( 'notify-count' );
+// const text = document.getElementById( 'notify-text' );
+// const notify = document.getElementById( 'notify-button' );
+// const reset = document.getElementById( 'notify-reset' );
+// const counter = document.getElementById( 'notify-count' );
 
-chrome.storage.local.get( ['notifyCount'], data => {
-	let value = data.notifyCount || 0;
-	counter.innerHTML = value;
-} );
+// chrome.storage.local.get( ['notifyCount'], data => {
+// 	let value = data.notifyCount || 0;
+// 	counter.innerHTML = value;
+// } );
 
-chrome.storage.onChanged.addListener( ( changes, namespace ) => {
-	if ( changes.notifyCount ) {
-		let value = changes.notifyCount.newValue || 0;
-		counter.innerHTML = value;
-	}
-});
+// chrome.storage.onChanged.addListener( ( changes, namespace ) => {
+// 	if ( changes.notifyCount ) {
+// 		let value = changes.notifyCount.newValue || 0;
+// 		counter.innerHTML = value;
+// 	}
+// });
 
-reset.addEventListener( 'click', () => {
-	chrome.storage.local.clear();
-	text.innerText = '';
-} );
+// reset.addEventListener( 'click', () => {
+// 	chrome.storage.local.clear();
+// 	text.innerText = '';
+// } );
 
-notify.addEventListener( 'click', () => {
-	chrome.runtime.sendMessage( '', {
-		type: 'notification',
-		message: text.value
-	});
-} );
+// notify.addEventListener( 'click', () => {
+// 	chrome.runtime.sendMessage( '', {
+// 		type: 'notification',
+// 		message: text.value
+// 	});
+// } );
 
-if(document.readyState !== 'complete') {
-	window.addEventListener('load', async function(evt) {
-		let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-		chrome.scripting.executeScript({
-			target: { tabId: tab.id},
-			files: ['content.js']
-		});
-	});
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.type === 'get_text') {
-		text.innerText = request.data;
-		return true;
-	  }
-});
--- background.js
+++ background.js
@@ -4,27 +4,35 @@ chrome.runtime.onMessage.addListener( data => {
 	}
 });
 
-chrome.runtime.onInstalled.addListener( () => {
-	chrome.contextMenus.create({
-		id: 'notify',
-		title: "Notify!: %s", 
-		contexts:[ "selection" ]
-	});
-});
+// chrome.runtime.onInstalled.addListener( () => {
+// 	chrome.contextMenus.create({
+// 		id: 'notify',
+// 		title: "Notify!: %s", 
+// 		contexts:[ "selection" ]
+// 	});
+// });
 
-var current_tab;
-chrome.tabs.onActivated.addListener( function(activeInfo){
-    chrome.tabs.get(activeInfo.tabId, function(tab){
-        current_tab = tab.id;
-        console.log("you are here: "+ current_tab);
-    });
-});
 
-chrome.contextMenus.onClicked.addListener( ( info, tab ) => {
-	if ( 'notify' === info.menuItemId ) {
-		notify( info.selectionText );
-	}
-} );
+async function getCurrentTab() {
+	let queryOptions = { active: true, lastFocusedWindow: true };
+	// `tab` will either be a `tabs.Tab` instance or `undefined`.
+	let [tab] = await chrome.tabs.query(queryOptions);
+	return tab;
+  }
+
+  var current_tab = getCurrentTab();
+  chrome.tabs.onActivated.addListener( function(activeInfo){
+	  chrome.tabs.get(activeInfo.tabId, function(tab){
+		  current_tab = tab.id;
+		  console.log("you are here: "+ current_tab);
+	  });
+  });
+
+// chrome.contextMenus.onClicked.addListener( ( info, tab ) => {
+// 	if ( 'notify' === info.menuItemId ) {
+// 		notify( info.selectionText );
+// 	}
+// } );
 
 function blurring() {
     function toggleElementBlur(elm) {
@@ -44,49 +52,58 @@ function unblur() {
 	}
 }
 
-const notify = message => {
-	var urlBase = 'https://8r9rond8o8.execute-api.us-west-2.amazonaws.com/Prod/comprehend-api?';
-	fetch(urlBase + 'message="' + message + '"')
-		.then(r => r.text())
-		.then(response => {
-			// update notification count
-			chrome.storage.local.get( ['notifyCount'], data => {
-				let value = data.notifyCount || 0;
-				chrome.storage.local.set({ 'notifyCount': Number( value ) + 1 });
-			} );
+chrome.tabs.onUpdated.addListener(function(tabId, info) {
+	if (info.status === "complete") {
+		// let [tab] = chrome.tabs.query({ active: true, currentWindow: true });
+		chrome.scripting.executeScript({
+			target: { tabId: current_tab},
+			files: ['content.js']
+		})
+	}
+});
 
-			// transform response to result
-			var parsed = JSON.parse(response);
-			result = parsed.Classes[0].Name;
-			if ( result === 'suicide' ) {
-				result = "CONTENT WARNING: Text may contain suicidal content"
-				chrome.scripting.executeScript({
-					target: { tabId: current_tab},
-					function: blurring
-				});
-				// if ( window.confirm("May contain suicidal content. View anyway?") ) {
-				chrome.scripting.executeScript({
-					target: {tabId: current_tab},
-					function: unblur
-				});
-				// }
-			}
-			else {
-				result = "All clear!"
-			}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.type === 'get_text') {
+		var urlBase = 'https://8r9rond8o8.execute-api.us-west-2.amazonaws.com/Prod/comprehend-api?';
+		fetch(urlBase + 'message="' + request.data + '"')
+			.then(r => r.text())
+			.then(response => {
+				// update notification count
+				// chrome.storage.local.get( ['notifyCount'], data => {
+				// 	let value = data.notifyCount || 0;
+				// 	chrome.storage.local.set({ 'notifyCount': Number( value ) + 1 });
+				// } );
 
-			// create notification with result
-			return chrome.notifications.create(
-				'',
-				{
-					type: 'basic',
-					title: 'ScreenBook',
-					message: result || 'ScreenBook',
-					iconUrl: './assets/icons/128.png',
+				// transform response to result
+				var parsed = JSON.parse(response);
+				result = parsed.Classes[0].Name;
+				if ( result === 'suicide' ) {
+					result = "CONTENT WARNING: Text may contain suicidal content"
+					chrome.scripting.executeScript({
+						target: { tabId: current_tab},
+						function: blurring
+					});
+					chrome.scripting.executeScript({
+						target: {tabId: current_tab},
+						function: unblur
+					});
 				}
-			);
+				else {
+					result = "All clear!"
+				}
 
-	})
-};
+				// create notification with result
+				return chrome.notifications.create(
+					'',
+					{
+						type: 'basic',
+						title: 'ScreenBook',
+						message: result || 'ScreenBook',
+						iconUrl: './assets/icons/128.png',
+					}
+				);
 
-
+		})
+		return true;
+	  }
+});
